2e78b65c23ee3a22c8fc569e1ad58d2df0ae7385
tsakov <tsakov_e@abv.bg>
2015-04-03 19:23:06 +0300

    Implemented term creation and substitution.

---
+++ src/lambda/subs.clj
@@ -0,0 +1,32 @@
+(ns lambda.subs)
+
+(defn substitute [variable substitution term]
+  (case (:type term)
+    :variable (if (= variable term)
+                  substitution
+                  term)
+    :application (make-application
+                   (substitute variable substitution (:func term))
+                   (substitute variable substitution (:arg term)))
+    :abstraction (if (not= variable (:var term))
+                     (make-abstraction
+                       (:var term)
+                       (substitute variable substitution (:body term)))
+                     term)))
+
+(defn rename-bound-var [old-var new-var term]
+  (case (:type term)
+    :variable term
+    :application (make-application
+                   (rename-bound-var old-var new-var (:func term))
+                   (rename-bound-var old-var new-var (:arg term)))
+    :abstraction (if (= old-var (:var term))
+                     (make-abstraction
+                       new-var
+                       (rename-bound-var
+                         old-var
+                         new-var
+                         (substitute old-var new-var (:body term))))
+                     (make-abstraction
+                       (:var term)
+                       (rename-bound-var old-var new-var (:body term))))))
---
+++ src/lambda/terms.clj
@@ -0,0 +1,36 @@
+(ns lambda.terms
+  (:require [clojure.set :refer union]))
+
+(defn make-variable [var-name]
+  {:type :variable
+   :name var-name})
+
+(defn make-abstraction [variable term]
+  {:type :abstraction
+   :var variable
+   :body term})
+
+(defn make-application [function argument]
+  {:type :application
+   :func function
+   :arg argument})
+
+(defn free-vars [term]
+  (case (:type term)
+    :variable #{(:var term)}
+    :application (union (free-vars (:func term))
+                        (free-vars (:arg term)))
+    :abstraction (disj (free-vars (:body term))
+                       (:var term))))
+
+(defn bound-vars [term]
+  (case (:type term)
+    :variable #{}
+    :application (union (free-vars (:func term))
+                        (free-vars (:arg term)))
+    :abstraction (conj (free-vars (:body term))
+                       (:var term))))
+
+(defn vars [term]
+  (union (free-vars term)
+         (bound-vars term)))
