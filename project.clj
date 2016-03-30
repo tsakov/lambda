99c746737c1aaa6d27a6e3bb923caf4c08e8fe54
tsakov <tsakov_e@abv.bg>
2016-03-30 18:31:25 +0300

    Added type inference.

--- project.clj
+++ project.clj
@@ -4,9 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.logic "0.8.10"]
                  [org.clojure/core.incubator "0.1.3"]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [jline/jline "2.13"]]
+                 [jline/jline "2.13"]
+                 [debugger "0.1.7"]]
   :main ^:skip-aot lambda.core
   :jvm-opts   ["-Xss10M"]
   :target-path "target/%s"
---
+++ src/lambda/types.clj
@@ -0,0 +1,28 @@
+(ns lambda.types
+  (:require [clojure.core.logic :refer :all]))
+
+(declare typeo)
+
+(defn typeof
+  ([term]
+    (typeof term []))
+  ([term context]
+    (first
+      (run 1 [type]
+        (typeo term type context)))))
+
+(defn typeo [term type context]
+  (conde
+    [(== (:type term) :variable)
+     (membero [(:name term) type] context)]
+
+    [(== (:type term) :application)
+     (fresh [t]
+       (typeo (:func term) [t type] context)
+       (typeo (:arg term) t context))]
+
+    [(== (:type term) :abstraction)
+     (fresh [t1 t2 c]
+       (== type [t1 t2])
+       (conso [(:var term) t1] context c)
+       (typeo (:body term) t2 c))]))
